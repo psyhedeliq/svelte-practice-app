3e10e9d6de8648dbc8a4aa5b5042b4aeab7df1a0
Cornel Filip <c.filip@192-168-0-102.rdsnet.ro>
2020-04-12 21:53:28 +0300

    setup

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = "Cornel";
+  let age = 33;
+
+  const incrementAge = () => {
+    age += 1;
+  };
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change age</button>
