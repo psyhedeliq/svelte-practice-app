a073d84cbd5adeac3991b320bf3fecd4a3d4b21c
Cornel Filip <c.filip@192-168-0-102.rdsnet.ro>
2020-04-12 22:59:14 +0300

    two-way binding shortcut

--- src/App.svelte
+++ src/App.svelte
@@ -33,4 +33,5 @@
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change age</button>
 <!-- <button on:click={changeName}>Change name</button> -->
-<input type="text" value={name} on:input={nameInput} />
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
