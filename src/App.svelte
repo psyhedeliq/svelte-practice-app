60b2f9ca8f24571459d58c45d7db737f7e76f397
Cornel Filip <c.filip@192-168-0-102.rdsnet.ro>
2020-04-12 23:27:37 +0300

    create and style ContactCard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import ContactCard from "./ContactCard.svelte";
+
   let name = "Cornel";
   let age = 33;
 
@@ -35,3 +37,5 @@
 <!-- <button on:click={changeName}>Change name</button> -->
 <!-- <input type="text" value={name} on:input={nameInput} /> -->
 <input type="text" bind:value={name} />
+
+<ContactCard />
